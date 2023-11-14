39fbe0b8a3618aa49fd36705606f7cb36036ac03
rbarreror <fitael@gmail.com>
2023-11-14 17:40:40 +0100

    Add Result Tabs Loading

--- src/components/app/results/Results.jsx
+++ src/components/app/results/Results.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
@@ -8,44 +8,27 @@ import { useJob } from '../JobContext';
 import DataDistribution from './EDA/DataDistribution/DataDistribution';
 import PCA from './EDA/PCA/PCA';
 import { useDispatchResults, useResults } from '../ResultsContext';
+import { CircularProgress } from '@mui/material';
+import { useVars } from '@/components/VarsContext';
 
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`vertical-tabpanel-${index}`}
-            aria-labelledby={`vertical-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box sx={{ p: 3 }}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    );
-}
-
-TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-    return {
-        id: `vertical-tab-${index}`,
-        'aria-controls': `vertical-tabpanel-${index}`,
-    };
+const statusTemplate = {
+    'EDA_PCA': 'waiting', // waiting, ok, error
+    'MOFA': 'waiting',
+    //'LEIDEN': 'waiting',
+    //'rCCA': 'waiting',
+    //'DCA': 'waiting',
+    //'ENET': 'waiting'
 }
 
 export default function Results() {
 
     const { tabValue } = useResults()
     const dispatchResults = useDispatchResults();
+
+    const { API_URL } = useVars();
+
+    const fetchRef = useRef();
+    const [status, setStatus] = useState(statusTemplate);
 
     const [value, setValue] = useState(tabValue);
     const section = Math.floor(value);
@@ -54,8 +37,21 @@ export default function Results() {
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
-        dispatchResults({type: 'set-tab-value', value: newValue});
+        dispatchResults({ type: 'set-tab-value', value: newValue });
     };
+
+    const fetchStatus = async () => {
+        const res = await fetch(`${API_URL}/get_status/${jobID}`);
+        const resJson = await res.json();
+        
+        setStatus(resJson);
+        
+        if(
+            Object.keys(resJson).map(e => resJson[e] != 'waiting').every(e => e)
+        ) {
+            clearInterval(fetchRef.current);
+        }
+    }
 
     return (
         <>
@@ -74,37 +70,26 @@ export default function Results() {
                     <Tab
                         label="EXPLORATORY DATA ANALYSIS"
                         value={0.1}
-                        {...a11yProps(0)}
                         sx={{ mt: 2, p: 0, color: section == 0 ? '#1976d2' : '#00000099' }}
                     />
-                    {(true || section == 0) && <Tab label="DATA DISTRIBUTION" value={0.1} {...a11yProps(1)} sx={{ fontSize: 12, m: 0, p: 0, borderTop: '1px solid #cccccc' }} />}
-                    {(true || section == 0) && <Tab label="PCA" value={0.2} {...a11yProps(2)} sx={{ fontSize: 12, m: 0, p: 0, borderBottom: '1px solid #cccccc' }} />}
+                    {(true || section == 0) && <Tab label="DATA DISTRIBUTION" value={0.1} sx={{ fontSize: 12, m: 0, p: 0, borderTop: '1px solid #cccccc' }} />}
+                    {(true || section == 0) && <Tab label="PCA" value={0.2} sx={{ fontSize: 12, m: 0, p: 0, borderBottom: '1px solid #cccccc' }} />}
 
-                    <Tab label="MULTIOMICS FACTOR ANALYSIS" value={1.1} {...a11yProps(1)} sx={{ mt: 2 }} />
-                    <Tab label="COMMUNITY ANALYSIS" value={2.1} {...a11yProps(3)} sx={{ mt: 2 }} />
-                    <Tab label="REGULARIZED CANONICAL CORRELATION ANALYSIS" value={3.1} {...a11yProps(4)} sx={{ mt: 2 }} />
-                    <Tab label="DIFFERENTIAL CORRELATION ANALYSIS" value={4.1} {...a11yProps(5)} sx={{ mt: 2 }} />
-                    <Tab label="ELASTIC NET" value={5.1} {...a11yProps(6)} sx={{ mt: 2 }} />
+                    <Tab label={<Typography><CircularProgress size={12} thickness={2} /> MULTIOMICS FACTOR ANALYIS</Typography>} value={1.1} sx={{ mt: 2 }} disabled={true} />
+                    <Tab label="COMMUNITY ANALYSIS" value={2.1} sx={{ mt: 2 }} disabled />
+                    <Tab label="REGULARIZED CANONICAL CORRELATION ANALYSIS" value={3.1} sx={{ mt: 2 }} disabled />
+                    <Tab label="DIFFERENTIAL CORRELATION ANALYSIS" value={4.1} sx={{ mt: 2 }} disabled />
+                    <Tab label="ELASTIC NET" value={5.1} sx={{ mt: 2 }} disabled />
                 </Tabs>
 
                 <Box sx={{ width: '85%', borderTop: '1px solid #cccccc' }}>
                     {value == 0.1 && <Box sx={{ p: 1 }}><DataDistribution /></Box>}
                     {value == 0.2 && <Box sx={{ p: 1 }}><PCA /></Box>}
-                    <TabPanel value={value} index={1.1}>
-                        MOFA
-                    </TabPanel>
-                    <TabPanel value={value} index={2.1}>
-                        Community Analysis
-                    </TabPanel>
-                    <TabPanel value={value} index={3.1}>
-                        rCCA
-                    </TabPanel>
-                    <TabPanel value={value} index={4.1}>
-                        Differential Correlation Analysis
-                    </TabPanel>
-                    <TabPanel value={value} index={5.1}>
-                        Elastic Net
-                    </TabPanel>
+                    {value == 1.1 && <Box sx={{ p: 1 }}>MOFA</Box>}
+                    {value == 2.1 && <Box sx={{ p: 1 }}>Community Analysis</Box>}
+                    {value == 3.1 && <Box sx={{ p: 1 }}>rCCA</Box>}
+                    {value == 4.1 && <Box sx={{ p: 1 }}>Differential Correlation Analysis</Box>}
+                    {value == 5.1 && <Box sx={{ p: 1 }}>Elastic Net</Box>}
                 </Box>
             </Box>
         </>
